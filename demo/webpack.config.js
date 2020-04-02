12d0de35612c9a1b95b43c97d4d57c2c681e8e77
Martin Guillon <martin.guillon@akylas.fr>
2020-04-02 15:20:23 +0200

    test: demo app update

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -258,7 +258,6 @@ module.exports = env => {
                 { from: { glob: "fonts/**" } },
                 { from: { glob: "**/*.jpg" } },
                 { from: { glob: "**/*.png" } },
-                { from: { glob: "assets/*.cer" } },
             ], { ignore: [`${relative(appPath, appResourcesFullPath)}/**`] }),
             new nsWebpack.GenerateNativeScriptEntryPointsPlugin("bundle"),
             // For instructions on how to set up workers with webpack

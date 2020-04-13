705dd4295821af0ffa11f41b6056373f330b5fbe
Martin Guillon <martin.guillon@akylas.fr>
2020-04-13 11:32:38 +0200

    test: demo app show async, cancel, progress

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,94 +1,165 @@
-import * as Https from 'nativescript-https';
-import * as Observable from 'tns-core-modules/data/observable';
-import * as fs from 'tns-core-modules/file-system';
-import * as dialogs from 'tns-core-modules/ui/dialogs';
-import * as Page from 'tns-core-modules/ui/page';
+import * as Https from "nativescript-https";
+import * as Observable from "tns-core-modules/data/observable";
+import * as fs from "tns-core-modules/file-system";
+import * as dialogs from "tns-core-modules/ui/dialogs";
+import * as Page from "tns-core-modules/ui/page";
 
-export function onNavigatingTo(args: Page.NavigatedData) {
-  let page = args.object as Page.Page;
-  page.bindingContext = Observable.fromObject({enabled: false});
+let page;
+let viewModel;
+export function pageLoaded(args: Page.NavigatedData) {
+    page = args.object as Page.Page;
+    viewModel = Observable.fromObject({
+        enabled: false,
+        progress: 0,
+        currentRequest: null,
+    });
+    page.bindingContext = viewModel;
 }
 
-function getRequest(url: string, allowLargeResponse = false) {
-  Https.request(
-      {
+function createRequest(
+    url: string,
+    options?: Partial<Https.HttpsRequestOptions>
+) {
+    return Https.createRequest({
+        useLegacy: true,
         url,
-        method: 'GET',
+        method: "GET",
         timeout: 1,
-        allowLargeResponse
-      })
-      .then(response => console.log('Https.request response', response))
-      .catch(error => {
-        console.error('Https.request error', error);
-        dialogs.alert(error);
-      });
+        ...options,
+    });
 }
 
-function postRequest(url: string, body: any) {
-  Https.request(
-      {
+function onError(error) {
+    console.error("Https.request error", error, error.stack);
+    dialogs.alert(error.toString());
+    page.bindingContext.currentRequest = null;
+    page.bindingContext.progress = 0;
+    return Promise.reject(error);
+}
+function getRequest(url: string, options?: Partial<Https.HttpsRequestOptions>) {
+    return Https.request({
+        useLegacy: true,
         url,
-        method: 'POST',
-        body
-      })
-      .then(response => console.log('Https.request response', response))
-      .catch(error => {
-        console.error('Https.request error', error);
-        dialogs.alert(error);
-      });
+        method: "GET",
+        timeout: 1,
+        ...options,
+    })
+        .then((response) => {
+            page.bindingContext.currentRequest = null;
+            page.bindingContext.progress = 0;
+            console.log("Https.request response", response);
+            return response;
+        })
+        .catch(onError);
+}
+
+function postRequest(
+    url: string,
+    options?: Partial<Https.HttpsRequestOptions>
+) {
+    return Https.request({
+        useLegacy: true,
+        url,
+        method: "POST",
+        ...options,
+    })
+        .then((response) => console.log("Https.request response", response))
+        .catch((error) => {
+            console.error("Https.request error", error);
+            dialogs.alert(error);
+        });
 }
 
 export function postHttpbin() {
-  postRequest('https://httpbin.org/post', {"foo": "bar", "baz": undefined, "plaz": null});
+    postRequest("https://httpbin.org/post", {
+        body: { foo: "bar", baz: undefined, plaz: null },
+    });
 }
 
 export function postHttpbinWithUTF8() {
-  Https.request(
-      {
-        url: 'https://httpbin.org/post',
-        method: 'POST',
-        body: {"foo": "bar", "baz": undefined, "plaz": null},
+    Https.request({
+        url: "https://httpbin.org/post",
+        method: "POST",
+        body: { foo: "bar", baz: undefined, plaz: null },
         headers: {
-          'Content-Type': "application/json; charset=utf-8",
-          'X-testing': "ok"
-        }
-      })
-      .then(response => console.log('Https.request response', response))
-      .catch(error => {
-        console.error('Https.request error', error);
+            "Content-Type": "application/json; charset=utf-8",
+            "X-testing": "ok",
+        },
+    }).catch((error) => {
+        console.error("Https.request error", error);
         dialogs.alert(error);
-      });
+    });
 }
 
 export function getHttpbin() {
-  getRequest('https://httpbin.org/get');
+    getRequest("https://httpbin.org/get");
+}
+
+export function cancelCurrentRequest() {
+    if (page.bindingContext.currentRequest) {
+        page.bindingContext.currentRequest.cancel();
+    }
+}
+export function getBigFile() {
+    const request = createRequest(
+        "http://ipv4.download.thinkbroadband.com/200MB.zip",
+        {
+            onProgress: (current, total) => {
+                page.bindingContext.progress = (current / total) * 100;
+            },
+        }
+    );
+    page.bindingContext.currentRequest = request;
+
+    return new Promise<any>((resolve, reject) => {
+        request.run(resolve, reject);
+    })
+        .then((response) => {
+            console.log("did get response");
+            let dir = fs.knownFolders.temp().getFile("200MB.zip");
+            return (response.content as Https.HttpsResponseLegacy).toFile(
+                dir.path
+            );
+            // console.log("did get response done");
+        })
+        .then(() => {
+            page.bindingContext.currentPromise = null;
+            console.log("did get response done");
+        })
+        .catch(onError);
 }
 
 export function getHttpbinLargeResponse() {
-  getRequest('https://httpbin.org/bytes/100000', true);
+    getRequest("https://httpbin.org/bytes/100000");
 }
 
 export function getMockbin() {
-  getRequest('https://mockbin.com/request');
+    getRequest("https://mockbin.com/request");
+}
+
+export function get404() {
+    getRequest("https://mockbin.com/reque2st");
 }
 
 export function enableSSLPinning(args: Observable.EventData) {
-  let dir = fs.knownFolders.currentApp().getFolder('assets');
-  let certificate = dir.getFile('httpbin.org.cer').path;
-  Https.enableSSLPinning({host: 'httpbin.org', commonName: "httpbin.org", certificate});
-  console.log('enabled');
+    let dir = fs.knownFolders.currentApp().getFolder("assets");
+    let certificate = dir.getFile("httpbin.org.cer").path;
+    Https.enableSSLPinning({
+        host: "httpbin.org",
+        commonName: "httpbin.org",
+        certificate,
+    });
+    console.log("enabled");
 }
 
 export function enableSSLPinningExpired(args: Observable.EventData) {
-  let dir = fs.knownFolders.currentApp().getFolder('assets');
-  let certificate = dir.getFile('httpbin.org.expired.cer').path;
-  Https.enableSSLPinning({host: 'httpbin.org', certificate});
-  console.log('enabled');
+    let dir = fs.knownFolders.currentApp().getFolder("assets");
+    let certificate = dir.getFile("httpbin.org.expired.cer").path;
+    Https.enableSSLPinning({ host: "httpbin.org", certificate });
+    console.log("enabled");
 }
 
 export function disableSSLPinning(args: Observable.EventData) {
-  Https.disableSSLPinning();
-  console.log('disabled');
+    Https.disableSSLPinning();
+    console.log("disabled");
 }
-
-

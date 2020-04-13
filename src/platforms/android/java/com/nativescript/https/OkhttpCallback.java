a058110f4b0a8a8c36d35285c087b36dbd7a0e7b
Martin Guillon <martin.guillon@akylas.fr>
2020-04-13 11:31:30 +0200

    chore: remove logs

--- src/platforms/android/java/com/nativescript/https/OkhttpCallback.java
+++ src/platforms/android/java/com/nativescript/https/OkhttpCallback.java
@@ -7,12 +7,12 @@ public class OkhttpCallback implements okhttp3.Callback {
     public void  onStringResponse(String responseString, int statusCode, okhttp3.Headers headers) {}
     public void onResponse(okhttp3.Call call, okhttp3.Response response ) throws java.io.IOException {
             String responseString = null;
-            Log.d(TAG, "onResponse1: " + ((android.os.Looper.getMainLooper().getThread() == java.lang.Thread.currentThread()) ? "1" : "0"));
             okhttp3.ResponseBody responseBody = response.body();
             responseString =  responseBody.string();
             responseBody.close();
-            Log.d(TAG, "onResponse2");
             onStringResponse(responseString, response.code(), response.headers());
     }
     public void onFailure(okhttp3.Call call, java.io.IOException e) {}
+
+
 }

4ecc3b4dea4a4d12beb15e21ab134af55d4746da
Martin Guillon <martin.guillon@akylas.fr>
2020-05-23 17:40:26 +0200

    expose clearCache method

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,6 +6,7 @@ export function disableSSLPinning();
 
 export function request(options: Https.HttpsRequestOptions): Promise<Https.HttpsResponse>;
 export function setCache(options?: Https.CacheOptions)
+export function clearCache()
 export function createRequest(opts: Https.HttpsRequestOptions): Https.HttpsRequest;
 
 export * from './https.common';

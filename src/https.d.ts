3490469cdc019265ec321ff9db37b94100f94b49
Martin Guillon <martin.guillon@akylas.fr>
2022-01-19 13:40:54 +0100

    fix: typings fix

--- src/https.d.ts
+++ src/https.d.ts
@@ -4,7 +4,11 @@ export function enableSSLPinning(options: Https.HttpsSSLPinningOptions);
 
 export function disableSSLPinning();
 
-export function request<U extends boolean, T = any>(options: Https.HttpsRequestOptions, useLegacy?: U): U extends true ? Promise<Https.HttpsResponseLegacy<T>> : Promise<Https.HttpsResponse<T>>;
+// export declare function request<T = any>(options: Https.HttpsRequestOptions): Promise<Https.HttpsResponse<Https.HttpsResponseLegacy<T>>>;
+export declare function request<T = any, U extends boolean = true>(
+    options: Https.HttpsRequestOptions,
+    useLegacy?: U
+): U extends true ? Promise<Https.HttpsResponse<Https.HttpsResponseLegacy<T>>> : Promise<Https.HttpsResponse<T>>;
 export function setCache(options?: Https.CacheOptions);
 export function clearCache();
 export function createRequest(opts: Https.HttpsRequestOptions): Https.HttpsRequest;

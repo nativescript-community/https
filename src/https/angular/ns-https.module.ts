e1893e4481793abecf32bb44ef6185c498935d2d
Ruslan Lekhman <lekhman112@gmail.com>
2023-05-01 15:58:24 -0600

    fix: angular code formatting & error handling

--- src/https/angular/ns-https.module.ts
+++ src/https/angular/ns-https.module.ts
@@ -2,54 +2,39 @@ import { NgModule, ModuleWithProviders, Optional, Inject, InjectionToken } from 
 import { HttpBackend } from '@angular/common/http';
 import { NativeScriptHttpClientModule } from '@nativescript/angular';
 import { HttpsSSLPinningOptions, enableSSLPinning } from '@nativescript-community/https';
-import {
-  HTTPS_REQUEST_DEFAULT_OPTIONS,
-  NativeScriptHttpXhrBackend,
-  HttpsRequestDefaultOptions
-} from './ns-http-xhr-backend';
+import { HTTPS_REQUEST_DEFAULT_OPTIONS, NativeScriptHttpXhrBackend, HttpsRequestDefaultOptions } from './ns-http-xhr-backend';
 import { ExcludedService } from './excluded.service';
 
 /** Page size injection token. */
-export const HTTPS_SSL_PINNING_OPTIONS
-  = new InjectionToken<HttpsSSLPinningOptions>('HTTPS_SSL_PINNING_OPTIONS');
+export const HTTPS_SSL_PINNING_OPTIONS = new InjectionToken<HttpsSSLPinningOptions>('HTTPS_SSL_PINNING_OPTIONS');
 
 @NgModule({
-  providers: [
-    ExcludedService,
-    NativeScriptHttpXhrBackend,
-    { provide: HttpBackend, useExisting: NativeScriptHttpXhrBackend },
-    { provide: HTTPS_REQUEST_DEFAULT_OPTIONS, useValue: { } },
-    { provide: HTTPS_SSL_PINNING_OPTIONS, useValue: { host: '', certificate: '' } }
-  ],
-  imports: [
-    NativeScriptHttpClientModule
-  ],
-  exports: [
-    NativeScriptHttpClientModule
-  ]
+    providers: [
+        ExcludedService,
+        NativeScriptHttpXhrBackend,
+        { provide: HttpBackend, useExisting: NativeScriptHttpXhrBackend },
+        { provide: HTTPS_REQUEST_DEFAULT_OPTIONS, useValue: {} },
+        { provide: HTTPS_SSL_PINNING_OPTIONS, useValue: { host: '', certificate: '' } }
+    ],
+    imports: [NativeScriptHttpClientModule],
+    exports: [NativeScriptHttpClientModule]
 })
 export class NativeScriptHttpsModule {
-  constructor(
-    @Optional()
-    @Inject(HTTPS_SSL_PINNING_OPTIONS)
-    defaults?: HttpsSSLPinningOptions
-  ) {
-    enableSSLPinning(defaults ?? { host: '', certificate: '' });
-  }
-  /**
-   * Creates and configures a module.
-   * @param defaults Https request default options.
-   * @returns A wrapper around an NgModule that associates it with the providers.
-   */
-  static forRoot(
-    defaults: HttpsRequestDefaultOptions & HttpsSSLPinningOptions = { host: '', certificate: '' }
-  ): ModuleWithProviders<NativeScriptHttpsModule> {
-    return {
-      ngModule: NativeScriptHttpsModule,
-      providers: [
-        { provide: HTTPS_REQUEST_DEFAULT_OPTIONS, useValue: defaults },
-        { provide: HTTPS_SSL_PINNING_OPTIONS, useValue: defaults }
-      ]
-    };
-  }
+    constructor(@Optional()@Inject(HTTPS_SSL_PINNING_OPTIONS) defaults?: HttpsSSLPinningOptions) {
+        enableSSLPinning(defaults ?? { host: '', certificate: '' });
+    }
+    /**
+     * Creates and configures a module.
+     * @param defaults Https request default options.
+     * @returns A wrapper around an NgModule that associates it with the providers.
+     */
+    static forRoot(defaults: HttpsRequestDefaultOptions & HttpsSSLPinningOptions = { host: '', certificate: '' }): ModuleWithProviders<NativeScriptHttpsModule> {
+        return {
+            ngModule: NativeScriptHttpsModule,
+            providers: [
+                { provide: HTTPS_REQUEST_DEFAULT_OPTIONS, useValue: defaults },
+                { provide: HTTPS_SSL_PINNING_OPTIONS, useValue: defaults }
+            ]
+        };
+    }
 }
